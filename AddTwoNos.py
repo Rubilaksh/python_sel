92cf9bc1e766270e083e2fceca1554c6381724a4
Rubilaksh <132240685+Rubilaksh@users.noreply.github.com>
2023-05-04 07:23:22 +0530

    Added comments for AddTwoNos.py
    Added comments for AddTwoNos.py

--- AddTwoNos.py
+++ AddTwoNos.py
@@ -1,3 +1,4 @@
+#function to add 2 nos
 def add_two_nos(firstNo, secondNo):
     result = firstNo+secondNo
     print("Addition of 2 nos is ",result)
@@ -7,8 +8,10 @@ def add_two_nos_return(firstNo, secondNo):
     result= firstNo+secondNo
     return result
 
-
+#function call
 add_two_nos(15.7,25.3)
 
 additionValue= add_two_nos_return(10,15)
+
+#Printing the value
 print("Addition of 2 nos is ",additionValue)
